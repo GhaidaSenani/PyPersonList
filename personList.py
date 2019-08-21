77f06bb5017886a2169430b658fc72754333d864
GhaidaSenani <46917360+GhaidaSenani@users.noreply.github.com>
2019-08-21 07:21:25 +0300

    Add files via upload

---
+++ personList.py
@@ -0,0 +1,37 @@
+class Person(object):
+    """__init__() functions as the class constructor"""
+
+    def __init__(self, name=None, job=None, nid=None):
+        self.name = name
+        self.job = job
+        self.nid = nid
+
+
+
+
+personList = []
+personList.append(Person("John", "teacher", "1234"))
+personList.append(Person("sara", "bicyclist", "8787"))
+personList.append(Person("fahad", "teacher", "8980"))
+personList.append(Person("ford", "planner", "2313"))
+personList.append(Person("Ghaida", "dentist", "9093"))
+personList.append(Person("Jimy", "coach", "9234"))
+
+import operator
+
+personList.sort(key=operator.attrgetter('job'))
+
+
+for person in personList:
+    print(" NID: "+ person.nid +" Name: "+person.name +" Job: "+person.job+"\n")
+
+
+choice = input("Enter a job to show  all persons in that job  \n teacher\n bicyclist \n planner \n dentist \n coach \n ")
+
+
+for person in personList:
+    if choice in person.job:
+       print(" NID: " + person.nid + " Name: " + person.name + " Job: " + person.job + "\n")
+
+
+
